9ffe626b3e13ea1284bb103423422174a5fc1aa6
Praveen96-matrix <praveenkumar96.n@gmail.com>
2026-07-12 21:47:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Docker container" This is first project
+    return "Hello from Docker containers" This is first project
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1800)
